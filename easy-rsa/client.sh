97a491a4991a479c9d31e4acbb5cd676097778e4
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-02-07 17:07:26 +0100

    add easy-rsa

---
+++ easy-rsa/client.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export EASYRSA=/etc/easy-rsa
+export EASYRSA_PKI="$(pwd)/pki"
+
+while [[ $# > 0 ]] ; do
+#    echo "set_var EASYRSA_REQ_CN '$1'" > "$EASYRSA_PKI/vars"
+    sed -e "s/\(set_var EASYRSA_REQ_CN\) .*/\1 '$1'/" -i "$EASYRSA_PKI/vars"
+    faketime -f "$(date --date='20200101 GMT' --rfc-3339=ns)" easyrsa --batch --days=3650 gen-req "$1" nopass
+    faketime -f "$(date --date='20200101 GMT' --rfc-3339=ns)" easyrsa --batch --days=3650 sign-req client "$1"
+    shift
+done
---
+++ easy-rsa/init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# <https://easy-rsa.readthedocs.io/en/latest/advanced/>
+
+export EASYRSA=/etc/easy-rsa
+export EASYRSA_PKI="$(pwd)/pki"
+
+cat > "$EASYRSA_PKI/vars" << EOF
+#set_var EASYRSA_ALGO ec
+#set_var EASYRSA_CURVE secp521r1
+#set_var EASYRSA_DIGEST "sha512"
+#set_var EASYRSA_NS_SUPPORT "yes"
+set_var EASYRSA_REQ_CN example
+EOF
+
+if [ ! -f dh.pem ] ; then
+    openssl dhparam 2048 > "$EASYRSA_PKI/dh.pem"
+    openvpn --genkey secret "$EASYRSA_PKI/ta.key"
+fi
+
+faketime -f "$(date --date='20200101 GMT' --rfc-3339=ns)" easyrsa --batch init-pki
+
+# ca
+#echo "set_var EASYRSA_REQ_CN '$1'" > "$EASYRSA_PKI/vars"
+sed -e "s/\(set_var EASYRSA_REQ_CN\) .*/\1 '$1'/" -i "$EASYRSA_PKI/vars"
+faketime -f "$(date --date='20200101 GMT' --rfc-3339=ns)" easyrsa --batch --days=3650 build-ca nopass
