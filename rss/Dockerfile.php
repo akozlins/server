8bf3c37825f72d4af091cd1b3f643e7d322b83fa
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-08-23 21:25:32 +0200

    cleanup

--- rss/Dockerfile.php
+++ rss/Dockerfile.php
@@ -21,6 +21,8 @@ RUN apk add --no-cache \
         -e 's/;\(error_log\) = .*/\1 = \/dev\/stderr/' \
         /etc/php8/php-fpm.conf
 
+RUN apk add --no-cache git
+
 RUN addgroup -g 1000 -S php && \
     adduser -u 1000 -S php -G php
 
