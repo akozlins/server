241890ec6055df9a0774d117d56f14929bfde7c4
Alexandr Kozlinskiy <akozlins@gmail.com>
2023-06-10 12:12:47 +0200

    [telegram] use flask.Blueprint

--- telegram/main.py
+++ telegram/main.py
@@ -6,8 +6,10 @@ import requests
 
 app = flask.Flask(__name__)
 
-# set web hook address:
-# `curl "https://api.telegram.org/bot${TOKEN}/setWebhook?url=https://${DOMAIN}/telegram/webhook"`
+@app.errorhandler(Exception)
+def exception_handler(error) :
+    print(error)
+    return '', 500
 
 def sendMessage(chat_id, text) :
     requests.post(f'https://api.telegram.org/bot{config.TOKEN}/sendMessage', json = {
@@ -15,26 +17,24 @@ def sendMessage(chat_id, text) :
         'text' : text,
     })
 
-@app.errorhandler(Exception)
-def exception_handler(error) :
-    print(error)
-    return '', 500
+bp = flask.Blueprint('bp', __name__)
 
-@app.route("/telegram/webhook", methods=[ 'GET' ])
+@bp.route("/webhook", methods=[ 'GET' ])
 def webhook_get() :
     return "get-ok"
 
-@app.route("/telegram/webhook", methods=[ 'POST' ])
+# set web hook address with:
+# `curl "https://api.telegram.org/bot${config.TOKEN}/setWebhook?url=https://${DOMAIN}/${config.PREFIX}/webhook"`
+@bp.route("/webhook", methods=[ 'POST' ])
 def webhook_post() :
     json = flask.request.get_json()
     print(json, flush=True)
 
-    if "message" not in json :
+    message = json.get("message", {})
+    if not message :
         return flask.Response('post-ok', status=200)
 
-    message = json['message']
-
-    chat_id = message['chat']['id']
+    chat_id = message.get("chat", {}).get("id", "")
     if chat_id not in config.CHATS :
         return flask.Response('post-ok', status=200)
 
@@ -44,5 +44,9 @@ def webhook_post() :
 
     return flask.Response('post-ok', status=200)
 
+app.register_blueprint(bp, url_prefix=config.PREFIX)
+
 if __name__ == "__main__" :
+    #import waitress
+    #waitress.serve(app, host="0.0.0.0", port=8080)
     app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
