4c9835f56aa10671c1900e2fee67bdcbc05f33e7
Alexandr Kozlinskiy <akozlins@gmail.com>
2023-08-11 08:25:20 +0200

    add/use TRAEFIK_SUBNET and INET_SUBNET

--- makefile.mk
+++ makefile.mk
@@ -19,21 +19,24 @@ logs :
 
 # traefik bridge network
 br-traefik :
+	source .env
 	docker network ls | grep -w traefik || \
 	$(SUDO) docker network create \
 	    --internal \
-	    --subnet="172.31.255.0/24" \
+	    --subnet="$$TRAEFIK_SUBNET.0/24" \
 	    --opt "com.docker.network.bridge.name=br-traefik" \
 	    --opt "com.docker.network.bridge.enable_icc=false" \
 	    traefik
 	# allow connections only from traefik container in traefik network
-	RULE="DOCKER-USER -i br-traefik -o br-traefik -s 172.31.255.254 -m conntrack --ctstate NEW -j ACCEPT"
+	RULE="DOCKER-USER -i br-traefik -o br-traefik -s $$TRAEFIK_SUBNET.254 -m conntrack --ctstate NEW -j ACCEPT"
 	sudo iptables -C $$RULE || sudo iptables -I $$RULE
 
 # internet bridge network
 br-inet :
+	source .env
 	docker network ls | grep -w inet || \
 	$(SUDO) docker network create \
+	    --subnet="$$INET_SUBNET.0/24" \
 	    --opt "com.docker.network.bridge.name=br-inet" \
 	    --opt "com.docker.network.bridge.enable_icc=false" \
 	    inet
