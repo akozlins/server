c49423feb7361c666399381149642e773d0fe2e4
Alexandr Kozlinskiy <akozlins@gmail.com>
2023-08-18 16:05:15 +0200

    cleanup

--- rss/run-fresh.sh
+++ rss/run-fresh.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
 set -euf
 
-cd /var/www/fresh || exit 1
+PIDS=""
 
+cd /var/www/fresh || exit 1
 # make needed dirs in ./data
 ./cli/prepare.php
 # init
 ./cli/reconfigure.php --auth_type none --default_user default --db-type sqlite
 # create user
 ./cli/user-info.php --user default || ./cli/create-user.php --user default
-
-PIDS=""
+# run update job
+( while sleep 1801 ; do
+    ./app/actualize_script.php
+done ) &
+PIDS="$PIDS $!"
 
 # run php-fpm server (0.0.0.0:9000)
 php-fpm --nodaemonize --force-stderr &
-PIDS="$PIDS $!"
-
-# run update job
-( while true ; do
-    php ./app/actualize_script.php
-    sleep 1801
-done ) &
 PIDS="$PIDS $!"
 
 trap "kill -s TERM --timeout 1000 KILL -- $PIDS" EXIT HUP INT TERM
--- rss/run-tt.sh
+++ rss/run-tt.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 set -euf
 
-cd /var/www/tt || exit 1
+PIDS=""
 
 export PGPASSWORD=$TTRSS_DB_PASS
 PGOPTS="--host=$TTRSS_DB_HOST --dbname=$TTRSS_DB_NAME --username=$TTRSS_DB_USER"
 
+cd /var/www/tt || exit 1
 # init
 ./update.php --update-schema=force-yes
 psql $PGOPTS -c "create extension if not exists pg_trgm"
-
-PIDS=""
+# run update job
+./update_daemon2.php &
+PIDS="$PIDS $!"
 
 # run php-fpm server (0.0.0.0:9000)
 php-fpm --nodaemonize --force-stderr &
-PIDS="$PIDS $!"
-
-# run update job
-./update_daemon2.php &
 PIDS="$PIDS $!"
 
 trap "kill -s TERM --timeout 1000 KILL -- $PIDS" EXIT HUP INT TERM
